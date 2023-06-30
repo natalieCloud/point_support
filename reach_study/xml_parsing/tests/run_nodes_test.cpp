337e6532f8049099995b633ef06ef70f1780789f
Natalie Chmura <ntchmura@gmail.com>
2023-06-30 14:10:30 -0400

    Add: Working CMakeLists.txt and package.xml!!!

---
+++ reach_study/xml_parsing/tests/run_nodes_test.cpp
@@ -0,0 +1,17 @@
+#include "../include/reach_parse/result_parsing.hpp"
+#include "../include/reach_parse/xml_parser.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_array.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <string>
+
+int main (int argv, char **argc) {
+
+    std::cout << "It Executes!!!" << std::endl;
+
+    return 0;
+}
